e2f74f5e26e39dc8047184664ea90538d84eac57
CaioGMorata <51411417+CaioGMorata@users.noreply.github.com>
2024-12-01 19:30:13 -0300

    Update Exercise_4.cpp
    English comments

--- Exercise_4.cpp
+++ Exercise_4.cpp
@@ -12,6 +12,8 @@ using namespace std;
 
 string ConcatRemove(const string& s, const string& t, int k) {
     // Procura o valor do prefixo comum
+  
+    // Look for the value of the common prefix
     int commonLength = 0;
     while (commonLength < s.size() && commonLength < t.size() && s[commonLength] == t[commonLength]) {
         commonLength++;
@@ -20,10 +22,17 @@ string ConcatRemove(const string& s, const string& t, int k) {
     // Calcula o mínimo de operações necessárias.
     // ((s.size() - commonLength) --> Necessário para remoção)
     // ((t.size() - commonLength) --> Necessário para inserção)
+  
+    // Calculates the minimum number of operations required.
+    // ((s.size() - commonLength) --> Required for removal)
+    // ((t.size() - commonLength) --> Required for insertion)
     int operationsNeeded = (s.size() - commonLength) + (t.size() - commonLength);
 
     // Verifica se o número exato de operações é possível
-    // Resto da divisão por 2 é sempre necessário, devido ao usuário poder remover e adicionar um caracter para equilibrar com o número de operações.
+    // Resto da divisão por 2 é sempre necessário, devido ao usuário poder remover e adicionar um caracter para equilibrar com o número exato de operações.
+
+    // Checks if the exact number of operations is possible
+    // Remainder of division by 2 is always necessary, because the user can remove and add a character to balance with the exact number of operations.
     if (k >= operationsNeeded && 
         (k - operationsNeeded) % 2 == 0 || k >= s.size() + t.size()) {
         return "yes";
@@ -34,6 +43,8 @@ string ConcatRemove(const string& s, const string& t, int k) {
 
 int main() {
     // Strings de entrada e operações.
+
+    // Input strings and operations.
     string s, t;
     int k;
     getline(cin, s);
@@ -41,6 +52,8 @@ int main() {
     cin >> k;
 
     // Resultado de Saída
+
+    // Output Result
     cout << ConcatRemove(s, t, k) << endl;
 
     return 0;
